91bf1ea932c58ab20636f1f7df8bae76244797cd
ramenoff <39674652+ramenoff@users.noreply.github.com>
2023-02-09 16:39:13 -0500

    Display a single black square.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,9 @@
+
+const container = document.querySelector(".container");
+
 var div = document.createElement("div");
 div.style.height = "20px";
 div.style.width = "20px";
-div.style.background = "black";
+div.style.background = "blagick";
+
+container.appendChild(div);
