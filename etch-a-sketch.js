c274c9b08b0da256dfe953e3c463d2d0895a367a
ramenoff <39674652+ramenoff@users.noreply.github.com>
2023-02-11 22:06:37 -0500

    Commented code for eventListener(). More info needed on how to access pre-existing divs.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,36 @@
 let rowNum = 16;
 let colNum = 16;
 
+// div.addEventListener("mouseover", function(){div.style.backgroundColor = "red";}); correct syntax
+function letDraw(square){
+
+    //square.style.backgroundColor = "red";
+    
+}
+
 function addGrid(rowNum, colNum){
 
     const container = document.querySelector(".sketchpad");
 
-        for(i = 0 ; i < rowNum * colNum ; i++ ){
+    for(i = 0 ; i < rowNum * colNum ; i++ ){
 
-            var div = document.createElement("div");    
-            div.className = "square";
-            container.style.gridTemplateRows = "repeat(" + rowNum + ", 1fr)";  
-            container.style.gridTemplateColumns = "repeat("+ colNum + ", 1fr)";
-            container.appendChild(div);
-            
-        }
+        var div = document.createElement("div");    
+        div.className = "square";
+        container.style.gridTemplateRows = "repeat(" + rowNum + ", 1fr)";  
+        container.style.gridTemplateColumns = "repeat("+ colNum + ", 1fr)";
+        container.appendChild(div);
+        
+    }
 
 }
 
 addGrid(rowNum, colNum);
+
+document.querySelectorAll(".sketchpad").forEach(square => letDraw());
+
+/*forEach(square => square.addEventListener("mouseover", function() {letDraw(square)}));*/
+//for each loop for Event Listeners
+
 
 
 
